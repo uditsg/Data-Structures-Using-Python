86e840bad33607b4cdba351ef7f472c1c583d6b4
Udit Sengupta <50023597+uditsg@users.noreply.github.com>
2019-07-08 23:49:13 +0530

    Adding the python implementation for Binary Tree Traversals

---
+++ Trees/Binary Tree/BinaryTree.py
@@ -0,0 +1,57 @@
+#Class to create a node for a Binary Tree
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+#Pre-Order Traversal : Root, Left, Right 
+def PreOrder(root):
+        
+    if root:
+        print(root.data)
+        PreOrder(root.left)
+        PreOrder(root.right)
+
+#In-Order Traversal : Left, Root, Right 
+def InOrder(root):
+
+    if root:
+        InOrder(root.left)
+        print(root.data)
+        InOrder(root.right)
+
+#Post-Order Traversal : Left, Right, Root
+def PostOrder(root):
+    if root:
+        PostOrder(root.left)
+        PostOrder(root.right)
+        print(root.data)
+
+
+#Driver Code
+if __name__ == '__main__':
+
+    Root = Node(1)
+    Root.left = Node(2)
+    Root.right = Node(3)
+    Root.left.left = Node(4)
+    Root.left.right = Node(5)
+
+    print("Pre-Order Traversal is :")
+    PreOrder(Root)
+
+    print("In-Order Traversal is :")
+    InOrder(Root)
+    
+    print("Post-Order Traversal is :")
+    PostOrder(Root)
+
+    
+        
+        
+        
+            
+    
+        
