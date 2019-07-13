1d450a4801fe89925c3c71422ecdb95ae8999d2b
Udit Sengupta <50023597+uditsg@users.noreply.github.com>
2019-07-13 15:20:03 +0530

    Adding modification to Insert method and implemented Search and Delete methods

--- Trees/Binary Search Tree/BinarySearchTree.py
+++ Trees/Binary Search Tree/BinarySearchTree.py
@@ -11,18 +11,16 @@ def Insert(root,key):
 
     if root is None:
         root = Node(key)
+        return root
 
     else:
         if key < root.key:
-            if root.left is None:
-                root.left = Node(key)
-            else:
-                Insert(root.left,key)
+            root.left = Insert(root.left,key)
+                
         else:
-            if root.right is None:
-                root.right = Node(key)
-            else:
-                Insert(root.right,key)
+            root.right = Insert(root.right,key)
+        return root
+            
 
 'InOrder Traversal'
 def InOrder(root):
@@ -31,17 +29,81 @@ def InOrder(root):
         print(root.key)
         InOrder(root.right)      
 
+def Search(root,key):
+    if root is None:
+        print("Key Not Found")
+    elif root.key == key:
+        print("Key Found")      
+
+    else:
+        if key < root.key:
+            Search(root.left,key)
+        else:
+            Search(root.right,key)
+'Used to find the next inorder successor to replace the deleted key in the Delete method'
+def NextSuccessor(root):
+    while root.left is not None:
+        root = root.left
+    return root
+
+'Deletes the matched node with the key supplied'      
+def Delete(root,key):
+    i=0
+    
+    #Base Case
+    if root is None:
+        print('we are here for',i)
+        i+=1
+        return root
+   #Iterate recursively over left child of the node if key < current node value
+    if key < root.key:
+            root.left = Delete(root.left,key)
+            return root
+
+    #Iterate recursively over right child of the node if key > current node value
+    elif key > root.key:
+            root.right = Delete(root.right,key)
+            return root
+        
+    #Delete logic :
+    else:
+        #The following if elif takes care when node either has one child or no child
+        if root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        elif root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        
+         #This logic takes care where node to be deleted will be replaced by the immediate inorder successor   
+        else:
+            temp = NextSuccessor(root.right)
+            root.key = temp.key
+            root.right = Delete(root.right,temp.key)
+            return root
+                
+        
 'Driver Function'       
 if __name__=='__main__':
-    root_node = Node(40)
-    Insert(root_node,30)
-    Insert(root_node,50)
-    Insert(root_node,80)
-    Insert(root_node,560)
-    Insert(root_node,34)
-    Insert(root_node,5)
+    root = Node(40)
+    root = Insert(root,30)
+    root = Insert(root,50)
+    root = Insert(root,80)
+    root = Insert(root,560)
+    root = Insert(root,34)
+    root = Insert(root,5)
 
-    InOrder(root_node)
+    Search(root,5)
+    Search(root,560)
+    Search(root,80)
+    Search(root,9985)
+    InOrder(root)
+    root = Delete(root,34)
+    print("After deleting")
+    
+    InOrder(root)
     
 
 
